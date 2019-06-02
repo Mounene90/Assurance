846c42abf9d0ef2b32dd680ee335d5111033bebc
MAMADOU NDIAYE <MAMADOU NDIAYE@NDIAYE>
2019-06-02 17:00:52 +0000

    okok

--- Assurance/src/main/java/sn/ucad/master/assurance/AssuranceApplication.java
+++ Assurance/src/main/java/sn/ucad/master/assurance/AssuranceApplication.java
@@ -8,7 +8,7 @@ public class AssuranceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AssuranceApplication.class, args);
-		System.out.println("bienvenue Mamadou Ndiaye");
+		
 	}
 
 }
