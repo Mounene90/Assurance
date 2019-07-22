f05e9dd2fbeb30c5ac90f30ccf18edb5de649b02
MAMADOU NDIAYE <MAMADOU NDIAYE@NDIAYE>
2019-07-22 00:57:27 +0000

    # WARNING: head commit changed in the meantime
    vcfd

--- Assurance/src/main/resources/static/js/scripts.js
+++ Assurance/src/main/resources/static/js/scripts.js
@@ -3,7 +3,7 @@
  */
 function CalculeBC()
 {
-    alert("je suis la");
-    document.getElementById('nomAssure').value=document.getElementById('nomSouscripteur').value;
-    document.getElementById('prenomAssure').value=document.getElementById('prenomSouscripteur').value;
+    alert('je suis la');
+    /*document.getElementById('nomAssure').value=document.getElementById('nomSouscripteur').value;
+    document.getElementById('prenomAssure').value=document.getElementById('prenomSouscripteur').value;*/
 }
